97021e3085d1c919a1076bf2b511cc6506117bfe
harshitkap00r <76745800+harshitkap00r@users.noreply.github.com>
2021-05-05 00:15:41 +0530

    done

--- router/auth.js
+++ router/auth.js
@@ -8,9 +8,6 @@ const authenticate = require("../middleware/authenticate");
 require('../db/conn');
 const User = require("../model/userSchema");
 
-router.get('/', (req, res) => {
-    res.send(`Hello world from the server rotuer js`);
-});
 
 
 
